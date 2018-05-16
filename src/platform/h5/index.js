4c7b02a4a94d1ddcc123f8365f4c64567dc1e276
蒋瑶 <landn172@gmail.com>
2018-05-16 16:22:11 +0800

    feat: add platform h5

--- src/platform/h5/index.js
+++ src/platform/h5/index.js
@@ -1,1 +1,22 @@
-export default {};
+console.log("invoke h5");
+let wx = {};
+
+// for debugger
+if (typeof Proxy !== "undefined") {
+  const _wx = wx;
+  wx = new Proxy(
+    {},
+    {
+      get(target, key, receiver) {
+        if (_wx[key]) return _wx[key];
+
+        // almost all wx[key] is a function
+        return () => {
+          console.warn(`invoke wx.${key}, but not achieve function`);
+        };
+      }
+    }
+  );
+}
+
+export default wx;
