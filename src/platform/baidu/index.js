752e2a7810ec2db8ef3446dc8315b315bc36982c
蒋瑶 <landn172@gmail.com>
2018-10-16 16:51:39 +0800

    feat: 添加适配百度代码

--- src/platform/baidu/index.js
+++ src/platform/baidu/index.js
@@ -1,1 +1,12 @@
+import { ProxyInvokePlatformApi, Extend } from "../../utils/index";
+
+const wx = {};
+const _swan = typeof swan !== "undefined" && swan;
+
+if (_swan) {
+  Extend(wx, _swan);
+}
+
 console.log('invoke baidu')
+
+export default ProxyInvokePlatformApi(wx)
